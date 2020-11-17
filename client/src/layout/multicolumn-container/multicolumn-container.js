0df88ecb6b71eafa3298c11b6ad3abfa8ed940c8
kylelambert001 <kylelambert001@gmail.com>
2020-11-17 19:32:35 +1030

    Created reusable multicolumn layout components

---
+++ client/src/layout/multicolumn-container/multicolumn-container.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./multicolumn-container.scss";
+
+function MulticolumnContainer({ children }) {
+  return <div className="multicolumn-container">{children}</div>;
+}
+
+export default MulticolumnContainer;
---
+++ client/src/layout/multicolumn-item/multicolumn-item.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./multicolumn-item.scss";
+
+function MulticolumnItem({ children }) {
+  return <div className="multicolumn-item">{children}</div>;
+}
+
+export default MulticolumnItem;
